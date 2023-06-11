10160862aaed2bbeb9eede51573342d95ec62432
rawi124 <rawia.ben.amira1997@gmail.com>
2023-06-12 00:16:06 +0200

    first

---
+++ tp1.py
@@ -0,0 +1,11 @@
+"""
+Prise en main de l'environnement Python/Numpy
+"""
+import numpy as np
+import matplotlib
+from matplotlib import pylab, mlab, pyplot
+
+from IPython.display import display
+from IPython.core.pylabtools import figsize, getfigs
+from pylab import *
+from numpy import *
